fb9f479d089bb421b3412136867c2d131dcb95b9
Jon Mullins <km4gig@gmail.com>
2018-10-20 22:29:11 -0400

    added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
     'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -42,18 +44,30 @@ gulp.task('compass', function() {
         .pipe(connect.reload())
 });
 
-    gulp.task('watch', function() {
-        gulp.watch(coffeeSources, ['coffee']);
-        gulp.watch(jsSources, ['js']);
-        gulp.watch('components/sass/*.scss', ['compass']);
+gulp.task('watch', function() {
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'builds/development/',
+        livereload: true  
     });
 
-    gulp.task('connect', function() {
-        connect.server({
-            root: 'builds/development/',
-            livereload: true  
-        });
+});
 
-    });
+gulp.task('html', function(){
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
+});
 
-    gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('json', function(){
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
